48a557fb285f21d5aab564256eb4b8c2960c4479
liu.meihong <liumei0606@126.com>
2016-04-07 08:15:08 +0800

    delete duplicate tests for scala

--- scala/src/test/scala/cn/codingstyle/hangman/HangmanSpec.scala
+++ scala/src/test/scala/cn/codingstyle/hangman/HangmanSpec.scala
@@ -5,7 +5,6 @@ import org.scalatest.Matchers._
 
 class HangmanSpec extends FunSpec {
   describe("start game") {
-
     def expect(word: String, length: Int, problem: String) {
       val hangman = new Hangman(word)
 
@@ -21,30 +20,6 @@ class HangmanSpec extends FunSpec {
       expect(word = "GOOGLE",length = 6, problem = "_OO__E")
       expect(word = "AEIOU", length = 5, problem = "AEIOU")
       expect(word = "",      length = 0, problem = "")
-    }
-
-    it("try guess GOOGLE") {
-      val hangman = new Hangman("GOOGLE");
-      hangman.tries should be(12)
-      hangman.used should be("AEIOU")
-      hangman.length should be(6)
-      hangman.problem should be("_OO__E")
-    }
-
-    it("try guess AEIOU") {
-      val hangman = new Hangman("AEIOU");
-      hangman.tries should be(12)
-      hangman.used should be("AEIOU")
-      hangman.length should be(5)
-      hangman.problem should be("AEIOU")
-    }
-
-    it("try guess empty world") {
-      val hangman = new Hangman("");
-      hangman.tries should be(12)
-      hangman.used should be("AEIOU")
-      hangman.length should be(0)
-      hangman.problem should be("")
     }
   }
 
